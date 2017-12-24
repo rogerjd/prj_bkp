5302f9324946716fb3ea2ba300a292ac057e11f9
roger <rjpd@netzero.com>
2017-12-23 21:03:42 -0500

    init

---
+++ prjbkp/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	fileList    = "FileList.txt"
+	bkpBasePath = "c:/prjs/"
+)
+
+var (
+	prjNum string
+)
+
+func main() {
+	fmt.Print("prjbkp")
+
+	if len(os.Args) != 2 {
+		log.Fatal("number args: please provide prj num")
+	}
+
+	prjNum = os.Args[1]
+	PrjPath, err := getPrdDirName(prjNum) //todo: PrjName
+	if err != nil {
+		log.Fatal(err)
+	}
+	BkpPath := bkpBasePath + PrjPath //fmt.Sprintf(BkpBasePath, prjNum)
+	BkpPath = BkpPath + "/bkps/" + makeBkpDirName()
+	err = os.Mkdir(BkpPath, os.ModeDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fl, err := os.Open(bkpBasePath + "/" + PrjPath + "/bkps/" + fileList)
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(fl)
+	for scanner.Scan() {
+		fIn, err := os.Open(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		fn := filepath.Base(scanner.Text())
+		fOut, err := os.OpenFile(BkpPath+"/"+fn, os.O_RDWR|os.O_CREATE, 666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		n, err := io.Copy(fOut, fIn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fIn.Close()
+		fOut.Close()
+		fmt.Printf("num bytes copied: %d", n)
+	}
+
+}
+
+func makeBkpDirName() string {
+	t := time.Now()
+	return t.Format("20060102150405")
+}
+
+func getPrdDirName(prjNum string) (string, error) {
+	fis, err := ioutil.ReadDir(bkpBasePath) //[]FileInfo  input in string, directory name
+	if err != nil {
+		log.Fatal(err)
+	}
+	count := 0
+	prjName := ""
+	for _, fi := range fis {
+		if fi.IsDir() {
+			count++
+			prjName = fi.Name()
+		}
+	}
+	if count != 1 {
+		return "", errors.New("Prd Dir not found or found multiple")
+	}
+	return prjName, nil
+}
